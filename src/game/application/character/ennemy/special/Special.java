c6d28512dc22f12cbe01ba8dbc8a32637c29ff6b
cleme <cleme@LAPTOP-0F39KE7A>
2019-12-03 13:25:17 +0100

    ajout avec tout les changement que j'ai fait (un bug les a pas commit)

---
+++ src/game/application/character/ennemy/special/Special.java
@@ -0,0 +1,21 @@
+package game.application.character.ennemy.special;
+/*
+ * 
+ * class pour les coup special utilisable une fois.
+ * 
+ */
+public abstract class Special {
+	
+	private boolean usable = true; 
+	
+	public boolean getUse() {
+		if (this.usable)
+			this.usable = !usable;
+		return usable;
+	}
+	
+	public abstract boolean specialSkill();
+	
+	
+	public abstract String toString();
+}
